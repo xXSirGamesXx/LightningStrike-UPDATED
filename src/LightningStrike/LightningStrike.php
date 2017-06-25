fd8e543ee01a1fc000b7185f71fd4f9be379fc75
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-06-25 16:57:55 -0400

    Add files via upload

--- src/LightningStrike/LightningStrike.php
+++ src/LightningStrike/LightningStrike.php
@@ -1,1 +1,139 @@
+<?php
 
+namespace LightningStrike;
+
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\entity\Entity;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+class LightningStrike extends PluginBase implements Listener{
+    /** @var Config */
+    private $lightning;
+    public function onLoad(){
+      $this->getLogger()->info(TextFormat::AQUA."Preparing For StartUp... STAND BY");
+    }
+    public function onEnable(){
+      $this->saveDefaultConfig();
+      $this->lightning = $this->getConfig()->getAll();
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->getLogger()->info(TextFormat::GREEN."Lightning Strike RUNNING Made By MichaelM04");
+    }
+    /**
+     * @param Player $p
+     * @param $height
+     */
+ /*   public function onRespawn(PlayerRepsawnEvent $e){
+	$p = $e->getPlayer();
+        $level = $p->getLevel();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->entityRuntimeId = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        foreach($level->getPlayers() as $pl){
+            $pl->dataPacket($light);
+        } 
+     }
+*/
+    public function onRespawn(PlayerRespawnEvent $e){
+	if($player->hasPermission("lightningstrike.use")){
+	$p = $e->getPlayer();
+        $level = $p->getLevel();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->entityRuntimeId = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        foreach($level->getPlayers() as $pl){
+            $pl->dataPacket($light);
+        } 
+     }
+	}
+    public function onQuit(PlayerQuitEvent $e){
+	if($player->hasPermission("lightningstrike.use")){
+	$p = $e->getPlayer();
+        $level = $p->getLevel();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->entityRuntimeId = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        foreach($level->getPlayers() as $pl){
+            $pl->dataPacket($light);
+        } 
+     }
+	}	
+    public function onJoin(PlayerJoinEvent $e){
+	if($player->hasPermission("lightningstrike.use")){
+	$p = $e->getPlayer();
+        $level = $p->getLevel();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->entityRuntimeId = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        foreach($level->getPlayers() as $pl){
+            $pl->dataPacket($light);
+        } 
+     }
+	}
+
+    public function onDeath(PlayerDeathEvent $e){
+		if($player->hasPermission("lightningstrike.use")){
+	$p = $e->getPlayer();
+        $level = $p->getLevel();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->entityRuntimeId = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        foreach($level->getPlayers() as $pl){
+            $pl->dataPacket($light);
+        } 
+     }
+	}
+  }
+ 
